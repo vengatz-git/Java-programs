75368bb38b3e7bf2b6cb9cd1f1b52e3e0aca0294
Vengat06 <148939435+Vengat06@users.noreply.github.com>
2024-11-19 12:45:45 +0530

    Task - 3  Date : 18/11/2024

---
+++ PrintNumbersTask.java
@@ -0,0 +1,49 @@
+package program_1;
+
+public class PrintNumbersTask {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("..................................................");
+		System.out.println("Printing the numbers 10 to 100 using while loop");
+		System.out.println("..................................................");
+
+		int n=10;
+		
+		while(n <= 100) {
+			
+			System.out.println(n);
+			
+			n = n + 10;
+		}
+		System.out.println("..................................................");
+		
+		
+		
+		
+//		//  Reverse 
+//		System.out.println("..................................................");
+//		System.out.println("Printing the numbers 100 to 10 using while loop");
+//		System.out.println("..................................................");
+//
+//		int1 n1=100;
+//		
+//		while(n1 >= 10) {
+//			
+//			System.out.println(n1);
+//			
+//			n1 -= 10;
+//		}
+//		System.out.println("..................................................");
+		
+	}
+
+}
+
+
+//write a program to print 10, 20, 30 ,40 ,50 ,60 ,70 ,80 ,90, 100 using while loop // Completed
+//write a program to print even and odd number separately by using If 				// Completed
+//write a program to print table of any number between 1 to 10 from the user on for loop 
+//write a java program to print the Armstrong number or not getting the input  from the user
+
